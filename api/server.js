9075d6e8ae481609581668c470f5159c01802e1c
utsav-baghela <utsavu858@gmail.com>
2025-04-03 15:32:13 +0530

    connecting to mongose and creating requireds folders

--- api/server.js
+++ api/server.js
@@ -6,9 +6,30 @@ const cookieParser = require("cookie-parser");
 
 
 const app = express();
+app.use(express.json());  
+//if we pass any data to body to retrive the data we have to use this  
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(cookieParser());
 
 
-const PORT = process.env.PORT || 5000;
+//MONGODO CONNECTION
+
+mongoose.connect(process.env.MONGODB_URL
+)
+.then(db => {
+    console.log("Database is connected Successfully") 
+    }).catch(e => {
+        console.log("MongoDb error", e);
+        
+    })
+
+
+    app.get("/test", (req, res) => {
+        res.send({id: 1, message:"hello Mikasa"})
+    })
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
     
