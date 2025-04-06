358c433767ca12a41cf7a8e389fe6d71e53f7b3f
utsav-baghela <utsavu858@gmail.com>
2025-04-06 20:18:53 +0530

    creating class schema

---
+++ api/models/class.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const classSchema = new mongoose.Schema({
+    school:{type:mongoose.Schema.ObjectId, ref:'School'},
+    class_text:{type:String, required:true},
+    class_num:{type:Number, required:true},
+    attendee:{type:mongoose.Schema.ObjectId, ref:"Teacher"},
+    createAt: {type:Date, default: new Date()}
+ });
+
+
+ module.exports = mongoose.model("Class", classSchema)
