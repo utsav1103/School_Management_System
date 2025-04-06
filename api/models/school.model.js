85c22d4e254c77d3198e5443e51b4fdfb04b1407
utsav-baghela <utsavu858@gmail.com>
2025-04-06 20:18:00 +0530

    creating school schema

---
+++ api/models/school.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const schoolSchema = new mongoose.Schema({
+    school_name:{type:String, required:true},
+    email:{type:String, required:true},
+    owner_name:{type:String, required:true},
+    school_image:{type:String, required:true},
+    password:{type:String, required:true},
+
+    createdAt :{type:Date, default:new Date()}
+})
+
+
+module.exports = mongoose.model("School", schoolSchema )
