fa6d54cdfe6b3afa5d5185bf3efe67f1d419c084
utsav-baghela <utsavu858@gmail.com>
2025-04-07 23:01:28 +0530

    creating school controller and adding school imgage logic

---
+++ api/controllers/school.controller.js
@@ -0,0 +1,21 @@
+//crud - create , read , update , delete
+
+// Authentication - School , student , teacher
+require("dotenv").config();
+const formidable = require("formidable");
+const School = require("../models/school.model");
+
+
+
+module.exports = {
+    registerSchool: async (req, res) => {
+        const form = new formidable.IncomingForm();
+        form.parse(req, (err, fields, files) => {
+            const photo = files.image[0];
+            let filepath = photo.filepath;
+            let originalFilename = photo.originalFilename.replace(" ","_");// photo one 
+            let newPath = path.join(__dirname, process.env.SCHOOL_IMAGE_PATH, originalFilename);
+        })
+    }
+
+}
