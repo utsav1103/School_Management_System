076601cd04fd0843229389d120b124f56c09bf02
utsav-baghela <utsavu858@gmail.com>
2025-04-08 14:36:28 +0530

    installing bcrypt for hassing the password

--- api/controllers/school.controller.js
+++ api/controllers/school.controller.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const formidable = require("formidable");
 const path = require("path");
 const fs = require("fs");
+const bcrypt = require("bcrypt");
 const School = require("../models/school.model");
 
 
@@ -17,6 +18,18 @@ module.exports = {
             let filepath = photo.filepath;
             let originalFilename = photo.originalFilename.replace(" ","_");// photo one 
             let newPath = path.join(__dirname, process.env.SCHOOL_IMAGE_PATH, originalFilename);
+
+            let photoData = fs.readFileSync(filepath);
+            fs.writeFileSync(newPath,photoData);
+
+            const salt = bcrypt.genSaltSync(10);
+            const newSchool = new School({
+                School_name: fields.school_name[0],
+                email:fields.email[0],
+                owner_name:fields.owner_name[0],
+                
+                password:fields.password[0]
+            })
         })
     }
 
