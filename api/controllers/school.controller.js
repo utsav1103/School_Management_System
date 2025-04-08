018feb01e1d3b2d20465b4550a50948710b5dac6
utsav-baghela <utsavu858@gmail.com>
2025-04-08 14:47:14 +0530

    completeing the registration logic

--- api/controllers/school.controller.js
+++ api/controllers/school.controller.js
@@ -12,8 +12,11 @@ const School = require("../models/school.model");
 
 module.exports = {
     registerSchool: async (req, res) => {
+        
+
+    try {
         const form = new formidable.IncomingForm();
-        form.parse(req, (err, fields, files) => {
+        form.parse(req, async(err, fields, files) => {
             const photo = files.image[0];
             let filepath = photo.filepath;
             let originalFilename = photo.originalFilename.replace(" ","_");// photo one 
@@ -23,14 +26,22 @@ module.exports = {
             fs.writeFileSync(newPath,photoData);
 
             const salt = bcrypt.genSaltSync(10);
+            const hashPassword = bcrypt.hashSync(fields.password[0], salt);
             const newSchool = new School({
                 School_name: fields.school_name[0],
                 email:fields.email[0],
                 owner_name:fields.owner_name[0],
                 
-                password:fields.password[0]
+                password:hashPassword,
             })
-        })
-    }
 
+            const savedSchool = await newSchool.save();
+            res.status(200).json({
+                success: true, data:savedSchool, message:"School si Registed Successfully"})
+        })
+    
+    }catch (error){
+         res.status(500).json({success:false, message:"School Registration Failed."})
+    }
+    }
 }
