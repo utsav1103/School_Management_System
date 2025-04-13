9634569c3a5b76d51da3b87c355f907822e2c0a7
utsav-baghela <utsavu858@gmail.com>
2025-04-13 22:37:45 +0530

    creating the subcomponents

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import School from './school/School.jsx'
 import './App.css'
+import Attendance from './school/components/attendance/Attendance.jsx'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         <Routes>
             {/*School Routes*/}
           <Route path='school' element={<School/>}>
+          <Route path='attendance' element={<Attendance/>}/>
 
           </Route>
 
---
+++ frontend/src/school/components/attendance/Attendance.jsx
@@ -0,0 +1,7 @@
+export default function Attendance() {
+    return (
+        <>
+            <h1>Attendance</h1>
+        </>
+    )
+}
