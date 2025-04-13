8ceccce6def9237f0bd994c0f3b544808f82ef8e
utsav-baghela <utsavu858@gmail.com>
2025-04-13 22:28:58 +0530

    checking school component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,34 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import School from './school/School.jsx'
 import './App.css'
 
 function App() {
 
   return (
     <>
-   
+      <BrowserRouter>
+        <Routes>
+            {/*School Routes*/}
+          <Route path='school' element={<School/>}>
+
+          </Route>
+
+            {/*Student Routes*/}
+          <Route>
+
+          </Route>
+
+            {/*TEacher Routes*/}
+          <Route>
+
+          </Route>
+
+            {/*Clients Routes*/}
+          <Route>
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/school/School.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+export default function School(){
+
+
+    return(
+
+        <>
+        <h1>School component</h1>
+        <Outlet/>
+        </>
+    
+)
+}
