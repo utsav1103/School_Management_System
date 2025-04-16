089da52cd85dcb976d4e75d5183fb116300ce9ff
utsav-baghela <utsavu858@gmail.com>
2025-04-16 23:02:22 +0530

    setting up the navbar

--- frontend/src/client/Client.jsx
+++ frontend/src/client/Client.jsx
@@ -1,12 +1,14 @@
 
 import { Outlet } from "react-router-dom"
+import Navbar from "./utility components/navbar/Navbar"
 export default function Client(){
 
 
 
     return (<>
 
-        <h1>Client main component</h1>
+        <Navbar/>    
+
         <Outlet/>
     </>
     )
