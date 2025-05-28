9a3804d4634f992e1a42ba68d87193ffc490744b
utsav-baghela <utsavu858@gmail.com>
2025-05-28 19:28:00 +0530

    adjusting the modal

--- frontend/src/client/components/home/gallery/Gallery.jsx
+++ frontend/src/client/components/home/gallery/Gallery.jsx
@@ -1,58 +1,74 @@
-import * as React from 'react';
-import  Box from '@mui/material/Box';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import axios from "axios";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 export default function Gallery() {
-     const [open, setOpen] = React.useState(false);
-     const [selectdSchool, setSelectedSchool] = React.useState(null);
-  const handleOpen = (school) =>{ setOpen(true)
-     setSelectedSchool(school)};
-  const handleClose = () => {setOpen(false)
-    setSelectedSchool(null)
+  const [open, setOpen] = React.useState(false);
+  const [selectedSchool, setSelectedSchool] = React.useState(null);
+  const [schools, setSchools] = React.useState([]);
+  const handleOpen = (school) => {
+    setOpen(true);
+    setSelectedSchool(school);
   };
-    const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedSchool(null);
+  };
+  const style = {
+    position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  maxWidth: "90vw",
+  maxHeight: "90vh",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
-};
+  overflow: "auto",
+  };
+
+  React.useEffect(() => {
+    axios
+      .get(`http://localhost:3000/api/school/all`)
+      .then((resp) => {
+        console.log("School", resp);
+        setSchools(resp.data.schools);
+        // setMessage(resp.data.message);
+        // setMessageType("success");
+        // Formik.resetForm();
+      })
+      .catch((e) => {
+        // setMessage(e.response.data.message);
+        // setMessageType("error");
+        console.log("Error", e); //error handling
+      });
+  }, []);
+
   return (
     <Box>
-
-    <ImageList sx={{ width: '100%' , height: 'auto' }}>
-      {itemData.map((item) => (
-          <ImageListItem key={item.img}>
-          <img
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            alt={item.title}
-            loading="lazy"
-            onClick={()=>{handleOpen(item)}}
+      <ImageList sx={{ width: "100%", height: "auto" }}>
+        {schools.map((school) => (
+          <ImageListItem key={school.school_image}>
+            <img
+              srcSet={`./images/uploaded/school/${school.school_image}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              src={`./images/uploaded/school/${school.school_image}?w=248&fit=crop&auto=format`}
+              alt={school.title}
+              loading="lazy"
+              onClick={() => {
+                handleOpen(school);
+              }}
             />
-          <ImageListItemBar
-            title={item.title}
-            subtitle={<span>by: {item.author}</span>}
-            position="below"
-            />
-        </ImageListItem>
-      ))}
-    </ImageList>
+            <ImageListItemBar title={school.school_name} position="bselow" />
+          </ImageListItem>
+        ))}
+      </ImageList>
 
- 
-
-
-    <div>
-      <Button onClick={handleOpen}>Open modal</Button>
       <Modal
         open={open}
         onClose={handleClose}
@@ -67,81 +83,22 @@ export default function Gallery() {
             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
           </Typography>
           <img
-            srcSet={`${selectdSchool && selectdSchool.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            src={`${selectdSchool && selectdSchool.img}?w=248&fit=crop&auto=format`}
+            // srcSet={`./images/uploaded/school/${selectedSchool && selectedSchool.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            src={
+              selectedSchool &&
+              `./images/uploaded/school/${selectedSchool.school_image}`
+            }
             alt={"alt"}
-            
-            
-            />
+            style={{
+    maxWidth: "100%",
+    maxHeight: "80vh",
+    objectFit: "contain",
+    display: "block",
+    margin: "0 auto",
+  }}
+          />
         </Box>
       </Modal>
-    </div>
-  
-
-
-      </Box>
+    </Box>
   );
 }
-
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
-    author: '@bkristastucchio',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-    author: '@rollelflex_graphy726',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-    author: '@helloimnik',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-    author: '@nolanissac',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-    author: '@hjrc33',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    author: '@arwinneil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-    author: '@tjdragotta',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-    author: '@katie_wasserman',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-    author: '@silverdalex',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-    author: '@shelleypauls',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-    author: '@peterlaster',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-    author: '@southside_customs',
-  },
-];
