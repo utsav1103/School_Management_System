98fed5bb823a41cbaad66bb1e15ba32318e3a402
utsav-baghela <utsavu858@gmail.com>
2025-06-08 21:31:34 +0530

    starting the dashboard

--- frontend/src/school/components/dashboard/Dashboard.jsx
+++ frontend/src/school/components/dashboard/Dashboard.jsx
@@ -1,4 +1,20 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { baseApi } from "../../../environment"
+
 export default function Dashboard() {
+
+    const fetchSchool = () => {
+        axios.get(`${baseApi}/school/fetch-single`).then(resp => {
+           console.log(resp) 
+        }).catch(e => {
+            console.log("Error", e)
+        })
+    }
+
+    useEffect(()=> {
+fetchSchool();
+    },[])
     return (
         <>
             <h1>Dashboard</h1>
