79f19d0688566a734e6452a7854bc337c07e4b13
utsav-baghela <utsavu858@gmail.com>
2025-06-19 21:39:41 +0530

    updating the delete route in class router

--- api/routers/class.router.js
+++ api/routers/class.router.js
@@ -7,7 +7,7 @@ router.post("/create",authMiddleware(['SCHOOL']), createClass);
 router.get("/all",authMiddleware(['SCHOOL']) ,getAllClasses);
 
 router.patch("/update/:id",authMiddleware(["SCHOOL"]), updateClassWithId);// auth for update or not check
-router.get("/delete/:id",authMiddleware(["SCHOOL"]), deleteClassWithId);
+router.delete("/delete/:id",authMiddleware(["SCHOOL"]), deleteClassWithId);
 
 
 module.exports = router;
