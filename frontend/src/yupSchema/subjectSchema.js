a7ce83c595780f59a8bf3c266ef28af06466ced7
utsav-baghela <utsavu858@gmail.com>
2025-06-20 19:39:59 +0530

    creating sujbectschema

--- frontend/src/yupSchema/subjectSchema.js
+++ frontend/src/yupSchema/subjectSchema.js
@@ -1,5 +1,5 @@
 import * as yup from 'yup'
- 
+  
 export const subjectSchema = yup.object({
     subjet_name:yup.string().min(2, "Atleast 2 characters are required").required("Subject Name is required.."),
     subject_codename:yup.string().required("Subject Code is required.."),
