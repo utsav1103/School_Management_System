1492a5bd6e0e4a5b333641dadeb7dea7fcb88904
utsav-baghela <utsavu858@gmail.com>
2025-06-28 20:27:08 +0530

    fixing type error in studetnrouter

--- api/routers/student.router.js
+++ api/routers/student.router.js
@@ -1,11 +1,11 @@
 const express = require ("express");
 const authMiddleware = require("../auth/auth");
-const { getStudentsWithQuery, updateStudent, getStudentOwnData,getStudentsWithQuery,loginStudent, getStudentWithID, deleteStudentWithId } = require("../controllers/student.controller");
+const { getStudentsWithQuery, updateStudent, getStudentOwnData,loginStudent, getStudentWithID, deleteStudentWithId, registerStudent } = require("../controllers/student.controller");
 
 const router = express.Router();
 
-router.post("/register", re);
-router.get("/all",authMiddleware['SCHOOL'], getStudentsWithQuery);
+router.post("/register", registerStudent);
+router.get("/all",authMiddleware(['SCHOOL']), getStudentsWithQuery);
 router.post("/login", loginStudent);
 router.patch("/update",authMiddleware(["SCHOOL"]), updateStudent);// auth for update or not check
 router.get("/fetch-single",authMiddleware(["STUDENT"]), getStudentOwnData);
