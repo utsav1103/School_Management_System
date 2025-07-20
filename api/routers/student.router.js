b625e6876b6ad89235013717296b32b281d28753
utsav-baghela <utsavu858@gmail.com>
2025-07-20 14:30:17 +0530

    updating student router

--- api/routers/student.router.js
+++ api/routers/student.router.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/register",authMiddleware(['SCHOOL']), registerStudent);
 router.get("/fetch-with-query",authMiddleware(['SCHOOL']), getStudentsWithQuery);
 router.post("/login", loginStudent);
-router.patch("/update",authMiddleware(["SCHOOL"]), updateStudent);// auth for update or not check
+router.patch("/update/:id",authMiddleware(["SCHOOL"]), updateStudent);// auth for update or not check
 router.get("/fetch-single",authMiddleware(["STUDENT"]), getStudentOwnData);
 router.get("/fetch/:id", authMiddleware(['SCHOOL']), getStudentWithID);
 router.delete("/delete /:id", authMiddleware(['SCHOOL']), deleteStudentWithId);
