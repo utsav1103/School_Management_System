6ec53cbd59906f5b44b51aabc5f2e33628343a1b
utsav-baghela <utsavu858@gmail.com>
2025-08-04 20:01:57 +0530

    adding a router for get schedule with id

--- api/routers/schedule.router.js
+++ api/routers/schedule.router.js
@@ -1,12 +1,13 @@
 const express = require ("express");
 const authMiddleware = require("../auth/auth");
-const { createSchedule, getSchedulesWithClass, updateScheduleWithId, deleteScheduleWithId } = require("../controllers/schedule.controller");
+const { createSchedule, getSchedulesWithClass, updateScheduleWithId, deleteScheduleWithId, getSchedulesWithId } = require("../controllers/schedule.controller");
 
 const router = express.Router();
 
 router.post("/create",authMiddleware(['SCHOOL']), createSchedule);
 router.get("/fetch-with-class/:id",authMiddleware(['SCHOOL']) ,getSchedulesWithClass);
 
+router.get("/fetch/:id",authMiddleware(["SCHOOL"]), getSchedulesWithId);// auth for update or not check
 router.patch("/update/:id",authMiddleware(["SCHOOL"]), updateScheduleWithId);// auth for update or not check
 router.delete("/delete/:id",authMiddleware(["SCHOOL"]), deleteScheduleWithId);
 
