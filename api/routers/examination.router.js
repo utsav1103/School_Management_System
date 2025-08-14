d06375fe805ee133bfdcb300dfc03ce65b01e48f
utsav-baghela <utsavu858@gmail.com>
2025-08-14 21:07:56 +0530

    creating examination router file

---
+++ api/routers/examination.router.js
@@ -0,0 +1,16 @@
+const express = require ("express");
+const authMiddleware = require("../auth/auth");
+const { newExamination, getAllExaminations, getExaminationsByClass, updateExaminationWithID, deleteExaminationWithId } = require("../controllers/examination.controller");
+
+
+const router = express.Router();
+
+router.post("/create",authMiddleware(['SCHOOL']), newExamination);
+router.get("/all",authMiddleware(['SCHOOL']) ,getAllExaminations);
+router.get("/class/:id",authMiddleware(['SCHOOL','TEACHER','STUDENT']) ,getExaminationsByClass);
+
+router.patch("/update/:id",authMiddleware(["SCHOOL"]), updateExaminationWithID);// auth for update or not check
+router.delete("/delete/:id",authMiddleware(["SCHOOL"]), deleteExaminationWithId);
+
+
+module.exports = router;  
