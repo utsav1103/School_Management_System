d06c66f7d77f22e7d1e16230d7dddfc61294681c
utsav-baghela <utsavu858@gmail.com>
2025-08-14 21:03:36 +0530

    delete exam with id function

--- api/controllers/examination.controller.js
+++ api/controllers/examination.controller.js
@@ -63,10 +63,31 @@ module.exports = {
             const {date , subjectId , examType} = req.body;
             
             await Examination.findOneAndUpdate({_id:examinationId,school:schoolId}, {$set:{examDate:date,subject:subjectId,examType:examType}})
+            res.status(200).json({success:true, message:"Examination updated."
+
+            })
             
         } catch (error) {
-            res.status(500).json({success: false,message:"Error in updating Examinations."})
+            res.status(500).json({success: false,message:"Error in updating Examination."})
         
+        }
+    },
+
+    deleteExaminationWithId: async (req,res)=>{
+
+        try {
+            const schoolId = req.user.schoolId;
+            const examinationId = req.params.id;
+            await Examination.findOneAndDelete({_id:examinationId,school:schoolId})
+            res.status(200).json({success:true, message:"Examination Deleted."
+
+            })
+            
+
+        } catch (error) {
+            res.status(500).json({success: false,message:"Error in Deleting Examination."})
+        
+            
         }
     }
 
