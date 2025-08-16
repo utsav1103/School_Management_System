7d04c6a91572f097c99a2636d420605cb4528b5d
utsav-baghela <utsavu858@gmail.com>
2025-08-16 17:32:09 +0530

    adding the form for examination

--- frontend/src/school/components/examinations/Examinations.jsx
+++ frontend/src/school/components/examinations/Examinations.jsx
@@ -34,12 +34,11 @@ export default function Examinations() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
+            <TableCell align='right'>Exam Date</TableCell>
+            <TableCell align="right">Subject</TableCell>
+            <TableCell align="right">Exam Type</TableCell>
+            <TableCell align="right">Actions</TableCell>
+            </TableRow>
         </TableHead>
         <TableBody>
           {examinations.map((examination) => (
@@ -47,14 +46,13 @@ export default function Examinations() {
               key={examination.name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
+              <TableCell align='right' component="th" scope="row">
                 {examination.name}
               </TableCell>
               <TableCell align="right">{examination.calories}</TableCell>
               <TableCell align="right">{examination.fat}</TableCell>
               <TableCell align="right">{examination.carbs}</TableCell>
-              <TableCell align="right">{examination.protein}</TableCell>
-            </TableRow>
+              </TableRow>
           ))}
         </TableBody>
       </Table>
