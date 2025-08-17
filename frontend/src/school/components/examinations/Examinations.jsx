8af994ec13724945f5be630b4db63bf1b9946515
utsav-baghela <utsavu858@gmail.com>
2025-08-17 18:53:19 +0530

    styling the add new exam form

--- frontend/src/school/components/examinations/Examinations.jsx
+++ frontend/src/school/components/examinations/Examinations.jsx
@@ -6,7 +6,17 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Box, Button, TextField, Typography } from '@mui/material';
+//import { Box, Button, TextField, Typography } from '@mui/material';
+
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  FormControl,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useState } from 'react';
 //date
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -35,38 +45,100 @@ export default function Examinations() {
 
   return (
     <>
-    <Box
-      component="form"
-      noValidate
-      autoComplete="off"
-      onSubmit={Formik.handleSubmit}
-      //style 
+     <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+      bgcolor="#f4f6f8"
     >
-      <Typography variant='h4'>Add New Exam</Typography>
+      <Card
+        sx={{
+          width: "100%",
+          maxWidth: 500,
+          boxShadow: 6,
+          borderRadius: 4,
+          p: 2,
+        }}
+      >
+        <CardContent>
+          <Typography
+            variant="h4"
+            textAlign="center"
+            fontWeight="bold"
+            gutterBottom
+            sx={{ color: "primary.main" }}
+          >
+            Add New Exam
+          </Typography>
 
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker
-            label="Select Date"
-            value={Formik.values.date ? dayjs(Formik.values.date) : null}
-            onChange={(newValue) => Formik.setFieldValue("date", newValue)}
-            // slotProps={{
-            //   textField: {
-            //     fullWidth: true,
-            //     error: Boolean(Formik.touched.date && Formik.errors.date),
-            //     helperText: Formik.touched.date && Formik.errors.date,
-            //   },
-            // }}
-          />
-        </LocalizationProvider>
-        {Formik.touched.date && Formik.errors.date && (<p style={{color:"orange",textTransform:"capitalize"}}>{Formik.errors.date}</p>)}
-      
-      <TextField name='subject' onChange={Formik.handleChange} onBlur={Formik.handleChange} label="Subject" variant="filled" />
-      {Formik.touched.subject && Formik.errors.subject && (<p style={{color:"orange",textTransform:"capitalize"}}>{Formik.errors.subject}</p>)}
-      <TextField name='examType' onChange={Formik.handleChange} onBlur={Formik.handleChange} label="Exam Type" variant="filled" />
-      {Formik.touched.examType && Formik.errors.examType && (<p style={{color:"orange",textTransform:"capitalize"}}>{Formik.errors.examType}</p>)}
-   
-      <Button type='submit' variant='contained'>Submit</Button>
+          <Box
+            component="form"
+            noValidate
+            autoComplete="off"
+            onSubmit={Formik.handleSubmit}
+            display="flex"
+            flexDirection="column"
+            gap={3}
+          >
+            {/* Date Picker */}
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label="Select Date"
+                value={Formik.values.date ? dayjs(Formik.values.date) : null}
+                onChange={(newValue) => Formik.setFieldValue("date", newValue)}
+                slotProps={{
+                  textField: {
+                    fullWidth: true,
+                    error: Boolean(Formik.touched.date && Formik.errors.date),
+                    helperText: Formik.touched.date && Formik.errors.date,
+                  },
+                }}
+              />
+            </LocalizationProvider>
 
+            {/* Subject */}
+            <TextField
+              name="subject"
+              label="Subject"
+              variant="outlined"
+              fullWidth
+              onChange={Formik.handleChange}
+              onBlur={Formik.handleBlur}
+              error={Boolean(Formik.touched.subject && Formik.errors.subject)}
+              helperText={Formik.touched.subject && Formik.errors.subject}
+            />
+
+            {/* Exam Type */}
+            <TextField
+              name="examType"
+              label="Exam Type"
+              variant="outlined"
+              fullWidth
+              onChange={Formik.handleChange}
+              onBlur={Formik.handleBlur}
+              error={Boolean(Formik.touched.examType && Formik.errors.examType)}
+              helperText={Formik.touched.examType && Formik.errors.examType}
+            />
+
+            {/* Submit Button */}
+            <Button
+              type="submit"
+              variant="contained"
+              size="large"
+              sx={{
+                mt: 2,
+                borderRadius: 2,
+                textTransform: "none",
+                fontSize: "1rem",
+                py: 1.2,
+              }}
+            >
+              Submit
+            </Button>
+          </Box>
+        </CardContent>
+      </Card>
     </Box>
     
     <TableContainer component={Paper}>
