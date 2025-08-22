8c74c254a092fa5bb71912919376f43996e93610
utsav-baghela <utsavu858@gmail.com>
2025-08-22 21:15:21 +0530

    minor typo update

--- frontend/src/yupSchema/noticeSchema.js
+++ frontend/src/yupSchema/noticeSchema.js
@@ -2,6 +2,6 @@ import * as yup from 'yup';
 
 export const noticeSchema = yup.object({
     title:yup.string().required("Title is required"),
-    description:yup.string().required("Description is required"),
+    message:yup.string().required("message is required"),
     audience:yup.string().required("Audience is required"),
 })
