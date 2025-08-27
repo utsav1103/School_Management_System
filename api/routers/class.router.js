a798956f4731cebba3d520cbb3c83cdba7332823
utsav-baghela <utsavu858@gmail.com>
2025-08-27 17:24:31 +0530

    adding new route to class router or getting attende id

--- api/routers/class.router.js
+++ api/routers/class.router.js
@@ -1,12 +1,12 @@
 const express = require ("express");
 const authMiddleware = require("../auth/auth");
-const {createClass , getAllClasses, updateClassWithId , deleteClassWithId, getSingleClass} = require("../controllers/class.controller")
+const {createClass , getAllClasses, updateClassWithId , deleteClassWithId, getSingleClass, getAttendeeClass} = require("../controllers/class.controller")
 const router = express.Router();
 
 router.post("/create",authMiddleware(['SCHOOL']), createClass);
 router.get("/all",authMiddleware(['SCHOOL','TEACHER']) ,getAllClasses);
 router.get("/single/:id",authMiddleware(['SCHOOL']) ,getSingleClass);
-
+router.get("/attendee/:id",authMiddleware(['TEACHER'],getAttendeeClass))
 router.patch("/update/:id",authMiddleware(["SCHOOL"]), updateClassWithId);// auth for update or not check
 router.delete("/delete/:id",authMiddleware(["SCHOOL"]), deleteClassWithId);
 
