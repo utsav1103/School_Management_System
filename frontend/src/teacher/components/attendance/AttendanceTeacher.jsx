ab7cb625333a22dd1e32af656a0c017cad6b1a3c
utsav-baghela <utsavu858@gmail.com>
2025-08-28 14:01:03 +0530

    adding alert for the teacher with 0 classes assigned

--- frontend/src/teacher/components/attendance/AttendanceTeacher.jsx
+++ frontend/src/teacher/components/attendance/AttendanceTeacher.jsx
@@ -2,23 +2,28 @@ import { useEffect } from "react"
 import { baseApi } from "../../../environment"
 import axios from "axios"
 import { useState } from "react"
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material"
+import { FormControl, InputLabel, MenuItem, Select, Alert } from "@mui/material"
 
 export default  function AttendanceTeacher()
 {
     const [classes , setClasses] = useState([])
-    const [selectedClass , setSelectedClass] = useState(null);
+    const [selectedClass , setSelectedClass] = useState("");
+    const [loading , setLoading]= useState(true);
     const fetchAttendeeClass = async () =>{
-      
+       
       try {
         const response = await axios.get(`${baseApi}/class/attendee`)
         console.log("attende class response",response)
+
+        
         setClasses(response.data.data)
         
       } catch (error) {
         console.log("Error => fetching attendee class",error)
+      } finally{
+        setLoading(false);
       }
-    }
+    };
     useEffect(()=>{
         fetchAttendeeClass()
     },[])
@@ -27,8 +32,13 @@ export default  function AttendanceTeacher()
         <>
         <h1>Attendance Teacher </h1>
 
+        {!loading && classes.length === 0 &&(<Alert severity="warning" sx={{mb:2}}>
+          You are not assigned to any class now.
+        </Alert>)}
+        
+        {classes.length > 0 && (
 
-        <FormControl fullWidth variant="outlined">
+          <FormControl fullWidth variant="outlined">
                     <InputLabel id="class-select-label">Class</InputLabel>
                     <Select
                       labelId="class-select-label"
@@ -47,6 +57,10 @@ export default  function AttendanceTeacher()
                       ))}
                     </Select>
                   </FormControl>
+
+        )}
+
+        
         </>
     )
 }
